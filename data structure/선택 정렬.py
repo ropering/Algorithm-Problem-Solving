30891cd7f3a44db8cf6c3269e6d9b0dd9ceaf8ef
Myeong Gyun O <50795314+ropering@users.noreply.github.com>
2021-08-18 13:29:03 +0900

    21.08.18

--- data structure/선택 정렬.py
+++ data structure/선택 정렬.py
@@ -1,10 +1,13 @@
 # https://www.daleseo.com/sort-selection/
 
+
 def selection_sort(arr):
     for i in range(len(arr) - 1):
         min_idx = i
-        for j in range(i + 1, len(arr)):
-            if arr[j] < arr[min_idx]:
+        for j in range(i+1, len(arr)):
+            if arr[min_idx] > arr[j]:
                 min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-      
+        arr[min_idx], arr[i] = arr[i], arr[min_idx]
+    print(arr)
+
+selection_sort([5,2,6,4,3,4,6,7,8,9,3])
