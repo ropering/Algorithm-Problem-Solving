bdc044f9243c7a2c986f2f41ab4fd7951ff57efa
roper <bonoboss1028@gmail.com>
2021-10-08 23:39:53 +0900

    21.10.08

--- Programmers/로또의 최고 순위와 최저 순위.py
+++ Programmers/로또의 최고 순위와 최저 순위.py
@@ -1,7 +1,7 @@
 '''
 풀이 방법
-- for문으로 동일한 값이 존재할 때마다 count를 증가한다 -> best 변수에 저장
-- 0의 개수만큼 count를 증가한다 -> worst 변수에 저장
+- for문으로 동일한 값이 존재할 때마다 count를 증가한다 -> result 변수에 저장
+- 0의 개수를 -> erased 변수에 저장
 - 변수를 리스트에 넣어 return
 '''
 # == rank=[6,6,5,4,3,2,1]
@@ -25,7 +25,5 @@ def solution(lottos, win_nums):
     for num in lottos:
         if num in win_nums:
             result += 1
-        else:
-            result += 0
     
     return [get_score(result + erased), get_score(result)]
